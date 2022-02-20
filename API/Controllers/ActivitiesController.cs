cc23f001681bc85da063f9ae31351330756c7a28
viknhat <viknhat@gmail.com>
2022-02-20 15:34:44 +0530

    first commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
---
+++ Domain/Activity.cs
@@ -0,0 +1,12 @@
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }  
+        public bool BookingStatus { get; set; }
+        public bool Favorite { get; set; }    
+        public bool UserSelection { get; set; }  
+
+    }
+}
